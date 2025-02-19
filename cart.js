9a7ed1d356a653de4060dde0c74f29d615a117af
Davi <davi.cesarbt@gmail.com>
2025-02-19 17:01:32 -0300

    products objects created

---
+++ cart.js
@@ -0,0 +1,28 @@
+const products =[
+    
+    {
+        id: 1,
+        name: 'Teclado Mecânico',
+        category: "Periféricos",
+        price: 100,
+        image: "./images/teclado-mecanico.jpg",
+        quantidy: 2
+    },
+    {
+        id: 2,
+        name: 'Cadeira Gamer',
+        category: "Acessórios",
+        price: 500,
+        image: "./images/cadeira-gamer.jpg",
+        quantidy: 1
+    },
+    {
+        id: 3,
+        name: 'Headset',
+        category: "Periféricos",
+        price: 350,
+        image: "./images/cadeira-gamer.jpg",
+        quantidy: 8
+    },
+
+]
